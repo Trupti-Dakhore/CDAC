171f539904aefbf37944a1032c03a772f078102a
Trupti-Dakhore <iam.trupti.prasanna@gmail.com>
2024-08-22 15:30:50 +0530

    Add files via upload

---
+++ Logic Building Session/Assignment1/grade.java
@@ -0,0 +1,21 @@
+class Grade{
+	public static void main(String[]args){
+		int score=50;
+		if(score>=90){
+			System.out.println("grade is A");
+		}
+		else if(score>=80&&score<=89){
+			System.out.println("grade is B");
+		}
+		else if(score>=70&&score<=79){
+		System.out.println("grade is C");
+	    }
+		else if(score>=60&&score<=69){
+			System.out.println("grade is D");
+		}
+		else{
+			System.out.println("grade is F");
+		}
+	}
+}
+	 
