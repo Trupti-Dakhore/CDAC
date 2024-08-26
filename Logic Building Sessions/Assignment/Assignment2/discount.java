165146607cf9888ea32fef42f031bc64e999b03e
Trupti-Dakhore <iam.trupti.prasanna@gmail.com>
2024-08-26 09:55:59 +0530

    Add files via upload

---
+++ Logic Building Sessions/Assignment/Assignment2/discount.java
@@ -0,0 +1,14 @@
+class Discount{
+public static void main(String[]args){
+int pur1=400;
+if(pur1>=1000){
+	System.out.println("Discount is "+(pur1*0.2));
+	}
+else if(pur1>=500&&pur1<=999){
+	System.out.println("Discount is "+(pur1*0.1));
+}
+else if(pur1<500){
+	System.out.println("Discount is "+(pur1*0.05));
+}
+}
+}
