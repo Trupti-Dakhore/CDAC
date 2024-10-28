26c43d3741cbdb9af153594bb4c5a543cd3193c0
Trupti-Dakhore <iam.trupti.prasanna@gmail.com>
2024-10-28 15:32:24 +0530

    Add files via upload

---
+++ Web Programming Technology/Assignments/Assignment_3/Assignment3(1).js
@@ -0,0 +1,8 @@
+function useMe() {
+    const paragraph = document.getElementById("para");
+    paragraph.style.color = "purple";
+    paragraph.style.fontSize = "33px";
+    paragraph.style.fontFamily = "times new roman";
+    paragraph.style.fontWeight="bold";
+    paragraph.style.textDecoration="none";
+  }
